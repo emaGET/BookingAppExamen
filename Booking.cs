d4a08bd107476f30a43a033a89063bd0545fe775
emaGET <emanuelepini@getacademy.no>
2026-06-19 10:58:08 +0200

    booking

--- Booking.cs
+++ Booking.cs
@@ -1,5 +1,24 @@
 ﻿namespace BookingApp;
 public class Booking
 {
+    public int Id { get; }
+    public string CustomerName { get; } = "";
+    public string Room { get; } = "";
+    public DateOnly Date { get; }
+    public TimeOnly Start { get; }
+    public TimeOnly End { get; }
+    public string Description { get; } = "";
+    
+    public Booking(int id, string customerName, string room, DateOnly date, TimeOnly start, TimeOnly end, string description)
+    {
+        Id = id;
+        CustomerName = customerName;
+        Room = room;
+        Date = date;
+        Start = start;
+        End = end;
+        Description = description;
+    }
 
 }
+
